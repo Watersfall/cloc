f39a543b165954c9da34d9df49a0cafae1f1bd29
Chris <dontshoot262@yahoo.com>
2019-03-05 04:00:13 -0500

    Added population growth to turn change

---
+++ clocTurns/src/main/java/com/watersfall/clocturn/math/FoodCalc.java
@@ -0,0 +1,32 @@
+package com.watersfall.clocturn.math;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.SortedMap;
+
+public class FoodCalc
+{
+    public static final double POP_FOOD_COST = 1/1000d;
+    public static final double FOOD_PER_LAND = 1/200d;
+    
+    public static int getFoodProduction(ResultSet results) throws SQLException
+    {
+        return (int)(results.getInt("land") * FOOD_PER_LAND);
+    }
+    
+    public static int getStandardFoodCost(ResultSet results) throws SQLException
+    {
+        int pop = results.getInt("population");
+        return (int)(pop * POP_FOOD_COST);
+    }
+    
+    public static int getFoodProduction(SortedMap results) throws SQLException
+    {
+        return (int)(Integer.parseInt(results.get("land").toString()) * FOOD_PER_LAND);
+    }
+    
+    public static int getStandardFoodCost(SortedMap results) throws SQLException
+    {
+        return (int)(Integer.parseInt(results.get("population").toString()) * POP_FOOD_COST);
+    }
+}
