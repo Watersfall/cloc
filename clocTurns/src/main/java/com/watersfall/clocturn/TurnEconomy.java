b04d0d3742d427a5c35fa55382e8f3a63a4b4211
Chris <dontshoot262@yahoo.com>
2019-03-05 03:57:16 -0500

    Added population growth to turn change

--- clocTurns/src/main/java/com/watersfall/clocturn/TurnEconomy.java
+++ clocTurns/src/main/java/com/watersfall/clocturn/TurnEconomy.java
@@ -1,5 +1,6 @@
 package com.watersfall.clocturn;
 
+import com.watersfall.clocturn.math.PopGrowthCalc;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,11 +22,14 @@ public class TurnEconomy extends Turn
             while(results.next())
             {
                 PreparedStatement resources = connection.prepareStatement("UPDATE cloc SET rm=rm+?, oil=oil+?, mg=mg+? WHERE id=?");
+                PreparedStatement population = connection.prepareStatement("UPDATE cloc SET population=population+? WHERE id=?");
                 resources.setInt(1, results.getInt("mines") > 0 ? results.getInt("mines") : 0);
                 resources.setInt(2, results.getInt("wells") > 0 ? results.getInt("wells") : 0);
                 resources.setInt(3, results.getInt("industry") > 0 ? results.getInt("industry") : 0);
                 resources.setInt(4, results.getInt("id"));
+                population.setInt(1, (int)Math.floor(results.getInt("population") * PopGrowthCalc.getPopGrowth(results)));
                 resources.execute();
+                population.execute();
             }
         } 
         catch (SQLException ex) 
