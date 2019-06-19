04476c1174c4cc34755eb15babd142abeacdf3f2
Watersfall <dontshoot262@yahoo.com>
2019-06-19 00:09:00 -0400

    Re-added basic turn

--- clocturn/src/main/java/com/watersfall/clocturn/TurnEconomy.java
+++ clocturn/src/main/java/com/watersfall/clocturn/TurnEconomy.java
@@ -19,11 +19,17 @@ public class TurnEconomy extends Turn
 	{
 		try
 		{
-			ResultSet results = connection.createStatement().executeQuery("SELECT * FROM cloc");
-
-			while(results.next())
+			ResultSet resultsEconomy = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE).executeQuery("SELECT * FROM cloc_economy");
+			ResultSet resultsDomestic = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE).executeQuery("SELECT * FROM cloc_domestic");
+			while(resultsEconomy.next() && resultsDomestic.next())
 			{
-
+				resultsEconomy.updateInt("oil", resultsEconomy.getInt("oil_wells") + resultsEconomy.getInt("oil"));
+				resultsEconomy.updateInt("coal", resultsEconomy.getInt("coal_mines") + resultsEconomy.getInt("coal"));
+				resultsEconomy.updateInt("iron", resultsEconomy.getInt("iron_mines") + resultsEconomy.getInt("iron"));
+				resultsEconomy.updateInt("manufactured", resultsEconomy.getInt("civilian_industry") + resultsEconomy.getInt("manufactured"));
+				resultsDomestic.updateInt("population", (int)(resultsDomestic.getInt("population") + (resultsDomestic.getInt("population") * PopGrowthMath.getPopGrowth(resultsEconomy, resultsDomestic))));
+				resultsEconomy.updateRow();
+				resultsDomestic.updateRow();
 			}
 		}
 		catch(SQLException ex)
