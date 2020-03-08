c50d62159e821a7f2ded368202331a4c0a9bb5af
Watersfall <dontshoot262@yahoo.com>
2020-03-08 12:57:36 -0400

    Updated SETUP.sql

--- SQL Files/SETUP.sql
+++ SQL Files/SETUP.sql
@@ -7,11 +7,13 @@ CREATE TABLE cloc_login(
    password CHAR(60),
    register_ip VARCHAR(15),
    last_ip VARCHAR(15),
+   last_login BIGINT,
    sess CHAR(32)
 );
 
 CREATE TABLE cloc_main(
-	turn INT UNSIGNED DEFAULT 0
+	week BIGINT UNSIGNED DEFAULT 1,
+	day BIGINT UNSIGNED DEFAULT 1
 );
 
 CREATE TABLE production(
@@ -76,11 +78,11 @@ CREATE TABLE cloc_cities(
 	railroads INT DEFAULT 1,
 	ports INT DEFAULT 0,
 	barracks INT DEFAULT 0,
-	iron_mines INT UNSIGNED DEFAULT 2,
-	coal_mines INT UNSIGNED DEFAULT 2,
-	oil_wells INT UNSIGNED DEFAULT 0,
-	civilian_industry INT UNSIGNED DEFAULT 0,
-	military_industry INT UNSIGNED DEFAULT 0,
+	iron_mines INT UNSIGNED DEFAULT 5,
+	coal_mines INT UNSIGNED DEFAULT 5,
+	oil_wells INT UNSIGNED DEFAULT 2,
+	civilian_industry INT UNSIGNED DEFAULT 1,
+	military_industry INT UNSIGNED DEFAULT 1,
 	nitrogen_industry INT UNSIGNED DEFAULT 0,
 	universities INT UNSIGNED DEFAULT 0,
 	name VARCHAR(64),
@@ -169,12 +171,12 @@ CREATE TABLE cloc_tech(
 
 CREATE TABLE cloc_policy(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	manpower_policy TINYINT UNSIGNED DEFAULT 2,
-	manpower_change INT DEFAULT 0,
-	food_policy TINYINT DEFAULT 1,
-	food_change INT DEFAULT 0,
-	economy_policy TINYINT DEFAULT 2,
-	economy_change INT DEFAULT 0,
+	manpower_policy ENUM('DISARMED_MANPOWER', 'VOLUNTEER_MANPOWER', 'RECRUITMENT_MANPOWER', 'MANDATORY_MANPOWER', 'SCRAPING_THE_BARREL_MANPOWER') DEFAULT 'VOLUNTEER_MANPOWER',
+	food_policy ENUM('RATIONING_FOOD', 'NORMAL_FOOD', 'FREE_FOOD') DEFAULT 'NORMAL_FOOD',
+	economy_policy ENUM('CIVILIAN_ECONOMY', 'EXTRACTION_ECONOMY', 'INDUSTRY_ECONOMY', 'AGRARIAN_ECONOMY', 'WAR_ECONOMY') DEFAULT 'CIVILIAN_ECONOMY',
+	manpower_change BIGINT DEFAULT 0,
+	food_change BIGINT DEFAULT 0,
+	economy_change BIGINT DEFAULT 0,
 	FOREIGN KEY fk_policy (id) REFERENCES cloc_login(id) ON DELETE CASCADE
 );
 
@@ -182,9 +184,9 @@ CREATE TABLE cloc_war(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	attacker INT,
 	defender INT,
-	start INT,
+	start BIGINT,
 	winner INT,
-	end INT DEFAULT -1,
+	end BIGINT DEFAULT -1,
 	peace INT default -1,
 	FOREIGN KEY fk_attacker (attacker) REFERENCES cloc_login(id) ON DELETE CASCADE,
 	FOREIGN KEY fk_defender (defender) REFERENCES cloc_login(id) ON DELETE CASCADE
@@ -240,9 +242,25 @@ CREATE TABLE cloc_treaty_invites(
 CREATE TABLE cloc_declarations(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	sender INT,
-	sent INT,
+	sent BIGINT,
 	content VARCHAR(2048),
 	FOREIGN KEY fk_declarations (sender) REFERENCES cloc_login (id) ON DELETE CASCADE
 );
 
-INSERT INTO cloc_main (turn) VALUES (0);
+CREATE TABLE global_stats_history(
+	 week BIGINT,
+	 total_nations BIGINT,
+	 total_neutral_nations BIGINT,
+	 total_entente_nations BIGINT,
+	 total_central_powers_nations BIGINT,
+	 total_population BIGINT,
+	 total_soldiers BIGINT,
+	 total_civilian_factories BIGINT,
+	 total_military_factories BIGINT,
+	 total_universities BIGINT,
+	 total_iron_mines BIGINT,
+	 total_coal_mines BIGINT,
+	 total_oil_wells BIGINT
+);
+
+INSERT INTO cloc_main (week, day) VALUES (1, 1);
