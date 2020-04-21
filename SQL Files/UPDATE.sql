30dcf47030d5b1e5c03bbfb526ff6e99ba605520
Watersfall <dontshoot262@yahoo.com>
2020-04-21 16:16:21 -0400

    Allow policies to be changed on the first turn

--- SQL Files/UPDATE.sql
+++ SQL Files/UPDATE.sql
@@ -5,3 +5,10 @@ ALTER TABLE cloc_cities
     MODIFY COLUMN iron_mines INT DEFAULT 1,
     MODIFY COLUMN coal_mines INT DEFAULT 1,
     MODIFY COLUMN oil_wells INT DEFAULT 1;
+
+ALTER TABLE cloc_policy
+	MODIFY COLUMN manpower_change BIGINT DEFAULT -2,
+    MODIFY COLUMN food_change BIGINT DEFAULT -2,
+    MODIFY COLUMN economy_change BIGINT DEFAULT -2,
+    MODIFY COLUMN fortification_change BIGINT DEFAULT -2,
+    MODIFY COLUMN farming_subsidies_change BIGINT DEFAULT -2;
